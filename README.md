582d212a9331fb9674f862b345d9effd201400c4
KostadinHamanov <koci.hamanov@gmail.com>
2022-08-08 01:50:54 +0300

    Create initial project structure (#1)
    * Add initial project structure; Add luquibase setup
    
    * README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# citylist-server
+## City List Application
+
+Create an enterprise-grade "city list" application (it will stay there for years, will be extended and
+maintained) which allows the user to do the following:
+
+- browse through the paginated list of cities with the corresponding photos
+- search by the name
+- edit the city (both name and photo)
+
+Notes
+- initial list of cities should be populated using the attached cities.csv file
+- city addition, deletion and sorting are not in the scope of this task
+
+Technical clarification
+- Spring Boot
+- any build system
+-  any database
+- any frontend stack
+- Usage of Spring Data REST is prohibited
---
+++ src/main/java/com/citylist/server/entity/City.java
@@ -0,0 +1,49 @@
+package com.citylist.server.entity;
+
+import javax.persistence.*;
+
+@Entity(name = "city")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    private String photo;
+
+    public City() {
+    }
+
+    public City(Long id, String name, String photo) {
+        this.id = id;
+        this.name = name;
+        this.photo = photo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
