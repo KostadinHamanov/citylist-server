ec7bc8a0882afb80877ce4a70315943812b48dce
Kostadin Hamanov <koci.hamanov@gmail.com>
2022-08-09 00:23:30 +0300

    Extend GET API - search city by name

--- src/main/java/com/citylist/server/config/Constants.java
+++ src/main/java/com/citylist/server/config/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
 
     public static final int DEFAULT_PAGE_SIZE = 10;
     public static final int DEFAULT_PAGE_NUMBER = 0;
+    public static final String DEFAULT_CITY_NAME = "";
 }
